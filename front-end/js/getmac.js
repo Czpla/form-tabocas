ac4848903ea070731f166de4d207cddc44fd2237
Eduardo Czpla <edu.czpla@gmail.com>
2020-07-01 21:31:43 -0300

    use address

---
+++ front-end/js/getmac.js
@@ -0,0 +1,12 @@
+document.querySelector("button").onclick = function() {
+    alert("shit")
+
+    // var address = require('address');
+    // var getmac = address();
+    // // default interface 'eth' on linux, 'en' on osx.
+    // getmac.ip(); // '192.168.0.2'
+    // getmac.ipv6(); // 'fe80::7aca:39ff:feb0:e67d'
+    // getmac.mac(function(err, addr) {
+    //     console.log(addr); // '78:ca:39:b0:e6:7d'
+    // });
+}
