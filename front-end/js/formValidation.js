b0385045c942da11f725f180e4b2f84494851353
Eduardo Czpla <edu.czpla@gmail.com>
2020-07-06 20:33:36 -0300

    form-validation finish

---
+++ front-end/js/formValidation.js
@@ -0,0 +1,78 @@
+const fields = document.querySelectorAll('[required]')
+
+
+function validateField(field) {
+
+    function verifyErrors() {
+        let foundError = false
+
+        for (const error in field.validity) {
+            if (field.validity[error] && !field.validity.valid) {
+                foundError = error
+            }
+        }
+
+        return foundError
+    }
+
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: "Por favor, preencha este campo."
+            },
+            email: {
+                valueMissing: "E-mail é obrigatório.",
+                typeMismatch: "Por favor, preencha um e-mail válido."
+            }
+        }
+
+        return messages[field.type][typeError]
+    }
+
+    function setCustomMessage(message) {
+        const spanError = field.parentNode.parentNode.querySelector('span.error')
+
+        if (message) {
+            spanError.innerHTML = message
+        } else {
+            spanError.innerHTML = ""
+        }
+    }
+
+    return function() {
+
+        const erro = verifyErrors()
+
+        if (erro) {
+            const message = customMessage(erro)
+            field.parentNode.style.borderColor = "firebrick"
+            setCustomMessage(message)
+        } else {
+            field.parentNode.style.borderColor = "#8AC04B"
+            setCustomMessage()
+        }
+    }
+}
+
+
+function customValidation(event) {
+
+    const field = event.target
+    const validation = validateField(field)
+
+    validation()
+}
+
+
+for (field of fields) {
+    field.addEventListener('invalid', event => {
+        event.preventDefault()
+        customValidation(event)
+    })
+    field.addEventListener('blur', customValidation)
+}
+
+document.querySelector('form').addEventListener('submit', event => {
+    console.log('formulario enviado!')
+    event.preventDefault()
+})
--- front-end/js/getmac.js
+++ front-end/js/getmac.js
@@ -1,12 +1,13 @@
-document.querySelector("button").onclick = function() {
-    alert("shit")
+// document.querySelector("button").onclick = function() {
+//     alert("shit")
 
-    // var address = require('address');
-    // var getmac = address();
-    // // default interface 'eth' on linux, 'en' on osx.
-    // getmac.ip(); // '192.168.0.2'
-    // getmac.ipv6(); // 'fe80::7aca:39ff:feb0:e67d'
-    // getmac.mac(function(err, addr) {
-    //     console.log(addr); // '78:ca:39:b0:e6:7d'
-    // });
-}
+
+// }
+// var address = require('address');
+// // var getmac = address();
+// // default interface 'eth' on linux, 'en' on osx.
+// address.ip(); // '192.168.0.2'
+// address.ipv6(); // 'fe80::7aca:39ff:feb0:e67d'
+// address.mac(function(err, addr) {
+//     console.log(addr); // '78:ca:39:b0:e6:7d'
+// });
