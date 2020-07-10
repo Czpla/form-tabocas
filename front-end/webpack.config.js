af5afc261f0060249cfc9ba7e113cfc27597b216
czpla <edu.czpla@gmail.com>
2020-07-10 14:02:22 -0300

    changes webpack

--- front-end/webpack.config.js
+++ front-end/webpack.config.js
@@ -1,10 +1,15 @@
 const webpack = require('webpack')
+const modoDev = process.env.NODE_ENV !== 'production'
 
 module.exports = {
-    mode: 'development',
+    mode: modoDev ? 'development' : 'production',
     entry: './src/input.js',
     output: {
         filename: 'input.js',
         path: __dirname + '/js'
+    },
+    devServer: {
+        contentBase: "./",
+        port: 9000
     }
 }
